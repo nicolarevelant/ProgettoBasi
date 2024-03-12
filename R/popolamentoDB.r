4f91cf77c624d9ccaf2883ea16d6a5cc2ced537e
Nicola Revelant <nicolarevelant@outlook.com>
2024-03-12 18:46:33 +0100

    Aggiunta persone - popolamento in R

--- R/popolamentoDB.r
+++ R/popolamentoDB.r
@@ -1,8 +1,9 @@
 library("RPostgreSQL")
 
-# TODO: indirizziCondominio, elencoCausali
+# TODO: elencoIndirizzi, elencoCausali, elencoNominativi
 
-db <- dbConnect(dbDriver("PostgreSQL"), dbname="test", user="test")
+
+
 
 # Genera elenco condomini senza ammontareComplessivo (codice, CC, indirizzo)
 
@@ -13,6 +14,9 @@ condomini.indirizzo <- paste("via Roma ", sample.int(100, condomini.size, replac
 
 condomini <- data.frame(codice = condomini.codice, CC = condomini.CC, indirizzo = condomini.indirizzo)
 
+
+
+
 # Genera elenco spese (dataOra, condominio, importo, causale)
 
 dataMin <- as.numeric(as.POSIXct("1990-01-01 00:00:00"))
@@ -21,11 +25,14 @@ elencoCausali <- c("Causale 1", "Causale 2")
 
 spese.size <- 4500
 spese.dataOra <- as.character(as.POSIXct(sample(dataMin:dataMax, spese.size, replace=T)))
-spese.condominio <- sample(condomini.codice, spese.size, replace=T)
+spese.condominio <- sample(condomini.codice, spese.size, replace=T) # TODO: coppia (dataOra, condominio) deve essere univoca !!!
 spese.importo <- sample(100:3000, spese.size, replace=T)
 spese.causale <- sample(elencoCausali, spese.size, replace=T)
 
 spese <- data.frame(dataOra = spese.dataOra, condominio = spese.condominio, importo = spese.importo, causale = spese.causale)
+
+
+
 
 # Genera elenco appartamenti senza proprietario (numero, condominio, quotaAnnoCorrente, sommaPagata, telefono, superficie)
 
@@ -53,4 +60,23 @@ for (x in 1:10) {
 
 appartamenti.superficie <- sample(40:300, appartament.size)
 
-dbDisconnect(db)
+
+
+
+# Genera elenco persone senza appartamento in cui abita (cf, nome, dataNascita, indirizzo?)
+
+dataMin <- as.numeric(as.POSIXct("1990-01-01 00:00:00"))
+dataMax <- as.numeric(as.POSIXct("2023-12-31 23:59:59"))
+
+
+persone.size <- 1000
+persone.cf <- paste(sep="", "CF_", sample(1:1000000, persone.size))
+persone.nome <- "Mario Rossi" # TODO: dove prendo i nominativi??
+persone.dataNascita <- as.character(as.POSIXct(sample(dataMin:dataMax, spese.size, replace=T)))
+persone.indirizzo <- paste("via Roma ", sample.int(100, condomini.size, replace=TRUE))
+
+
+
+#db <- dbConnect(dbDriver("PostgreSQL"), dbname="test", user="test")
+
+#dbDisconnect(db)
