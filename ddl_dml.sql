788983c7e20b06d0c89455f8c4dfb8256913ac96
ffabionski <122169225+ffabionski@users.noreply.github.com>
2024-04-21 15:17:08 +0200

    aggiunto trigger indirizzo

--- ddl_dml.sql
+++ ddl_dml.sql
@@ -1,7 +1,7 @@
-create database GestioneCondomini;
+CREATE DATABASE GestioneCondomini;
 
 create table condominio(
-    codice integer primary key,
+    codice integer PRIMARY KEY,
     contoCorrente varchar(12) NOT NULL, -- ZEROFILL
     indirizzo varchar(50) NOT NULL,
     ammontareComplessivo numeric(6,2) DEFAULT 0.00
@@ -9,8 +9,8 @@ create table condominio(
 
 create table spesa (
     dataOra timestamp,
-    condominio integer references condominio (codice),
-    importo numeric(6,2) NOT NULL check (importo > 0.00),
+    condominio integer REFERENCES condominio (codice),
+    importo numeric(6,2) NOT NULL CHECK (importo > 0.00),
     causale varchar(50) NOT NULL,
     CONSTRAINT pk_spesa primary key (dataOra, condominio)
 );
@@ -19,28 +19,61 @@ create domain cf as varchar(16)
     CHECK (length(value) = 16);
 
 create table persona (
-    cf_ cf primary key,
+    cf_ cf PRIMARY KEY,
     nome varchar(20),
     indirizzo varchar(50),
-    numeroAppartamento integer,
-    condominio integer
+    numeroAppartamento integer NOT NULL,
+    condominio integer NOT NULL
 );
 
 create table appartamento (
     numero integer check ((numero >= 1) and (numero <= 50)),
-    condominio integer references condominio (codice),
-    quotaAnnoCorrente numeric(6,2) NOT NULL check (quotaAnnoCorrente >= 0),
+    condominio integer REFERENCES condominio (codice),
+    quotaAnnoCorrente numeric(6,2) NOT NULL CHECK (quotaAnnoCorrente >= 0),
     sommaPagata numeric(6,2) NOT NULL,
     telefono varchar(10) check (telefono SIMILAR TO '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
     superficie integer NOT NULL check (superficie >= 1),
     proprietario cf NOT NULL,
-    CONSTRAINT pk_appartamento primary key (numero, condominio)
+    CONSTRAINT pk_appartamento PRIMARY KEY (numero, condominio)
 );
 
 ALTER TABLE persona
-    ADD CONSTRAINT fk_persona_app foreign key (numeroAppartamento, condominio)
-    references appartamento(numero, condominio);
+    ADD CONSTRAINT fk_persona_app FOREIGN KEY (numeroAppartamento, condominio)
+    REFERENCES appartamento(numero, condominio);
 
 ALTER TABLE appartamento
-    ADD CONSTRAINT fk_app_persona foreign key (proprietario) references persona(cf_);
+    ADD CONSTRAINT fk_app_persona FOREIGN KEY (proprietario) REFERENCES persona(cf_);
 
+-- indirizzo proprietario è null SSE possiede l'appartamento (numeroAppartamento, condominio), nel quale ci abita
+CREATE OR REPLACE FUNCTION deriva_indirizzo_persona()
+RETURN trigger AS
+$$
+    DECLARE
+        indirizzo_derivato varchar(50);
+    BEGIN
+        PERFORM appartamento.numero, appartamento.condominio
+        FROM appartamento
+        WHERE appartamento.proprietario = new.cf_
+            AND appartamento.numero = new.numeroAppartamento
+            AND appartamento.condominio = new.condominio;
+
+        IF FOUND THEN
+            UPDATE persona
+            SET indirizzo = NULL
+            WHERE OLD.cf_ = NEW.cf_;
+        ELSE
+            SELECT c.indirizzo INTO indirizzo_derivato
+            FROM condominio AS c
+            WHERE c.codice = new.condominio;
+
+            UPDATE persona
+            SET indirizzo = indirizzo_derivato
+            WHERE OLD.cf_ = NEW.cf_;
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_ind_persona
+AFTER INSERT OR UPDATE ON persona
+FOR EACH ROW
+EXECUTE PROCEDURE deriva_indirizzo_persona();
